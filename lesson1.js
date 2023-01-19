885c0bdc24e29d8f180fd3e309155a34843023d5
Kunal <73818493+kunalS1467@users.noreply.github.com>
2023-01-19 23:51:34 +0530

    Create lesson1.js
    Javascript : lesson1 variables and printing

---
+++ lesson1.js
@@ -0,0 +1,12 @@
+var username="Kunal"
+var password="kunals"
+var uNo="u158043"
+console.log(`
+your username is ${username}
+your password is ${password}
+your unumber is ${uNo}
+`)
+const isLoggedin=true;
+if(isLoggedin){
+    console.log("bonjour! je suis Kunal")
+}
